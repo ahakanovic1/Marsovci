1303aceee387c994badd6c97ce43c2c9bc8b2386
ahakanovic1 <ahakanovic1@etf.unsa.ba>
2026-04-12 21:55:35 +0200

    Obrisano sve iz RMAApp

--- KVIZApp/app/src/main/java/ba/unsa/etf/rma/kvizapp/data/model/Grupa.kt
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.rma.kvizapp.data.model
-//data class je tu kao model koji predstavlja kako izgleda objekat Grupa
-
-data class Grupa(
-    val naziv : String,
-    val nazivPredmeta :String
-)
--- KVIZApp/app/src/main/java/ba/unsa/etf/rma/kvizapp/data/model/Predmet.kt
+++
@@ -1,6 +0,0 @@
-package ba.unsa.etf.rma.kvizapp.data.model
-
-data class Predmet(
-    val naziv: String,
-    val godina:Int
-)
